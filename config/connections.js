ff4e593b1dc0701234efca71d3a431777e13a507
Leelai Hayslett <Leelai.Hayslett@gmail.com>
2022-09-06 01:02:56 -0400

    connection.js file complete

--- config/connections.js
+++ config/connections.js
@@ -3,6 +3,9 @@ const {connect, connection} = require("mongoose");
 const connectionURI = 
 process.enc.MONGODB_URI || 'mongodb+srv://admin:<password>@cluster0.8rtlu1z.mongodb.net/test';
 
-connect 
+connect(connectionURI, {
+    userNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 module.exports =connection;
