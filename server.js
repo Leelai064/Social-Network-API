bbffe979cd54ff9ca9d1b4b76dad1075f2bebebf
Leelai Hayslett <Leelai.Hayslett@gmail.com>
2022-09-06 01:13:05 -0400

    server.js file complete

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 const routes = require('./routes');
 const express = require ("express");
-const DB = require ("");
+const DB = require ("./config/connections");
+const PORT = process.emitWarning.port || 3493;
+const app = express();
+
+app.use(routes);
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+Db.once( "open", () => {
+    app.list(PORT, () =>{
+        console.log(`API server is live on port ${PORT} !! Launch Complete!!`);
+    });
+});
